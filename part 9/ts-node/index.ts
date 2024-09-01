6d4d91ebb56cc906ef8fef89d5fa399d9096763b
toyaAoi <96932308+toyaAoi@users.noreply.github.com>
2024-09-01 12:04:02 +0530

    e9.6 - e9.7

--- part 9/ts-node/index.ts
+++ part 9/ts-node/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import calculateBmi from "./utils/bmiCalculator";
+import calculateExercises from "./utils/exerciseCalculator";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   return res.send("Hello Full Stack!");
@@ -22,6 +25,23 @@ app.get("/bmi", (req, res) => {
     height,
     bmi,
   });
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const {
+    daily_exercises,
+    target,
+  }: { daily_exercises: number[]; target: number } = req.body;
+
+  if (!daily_exercises || !target) {
+    return res.status(400).json({ error: "parameters missing" });
+  }
+
+  const exercises = daily_exercises.map((e: number) => Number(e));
+  const result = calculateExercises(target, exercises);
+
+  return res.json(result);
 });
 
 const PORT = 3000;
